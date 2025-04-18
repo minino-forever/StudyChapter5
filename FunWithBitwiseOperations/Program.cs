a50db13287fff767e5db2049f96dc8c5778c7cde
AlexDev <minino-forever@rambler.ru>
2025-04-18 11:48:31 +0700

    add project FunWithBitwiseOperations

---
+++ FunWithBitwiseOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FunWithBitwiseOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========Fun With Bitwise Operations=========\n");
+
+            Console.WriteLine($"6 & 4 = {6 & 4} | {Convert.ToString((6 & 4), 2)}");
+
+            Console.WriteLine($"6 | 4 = {6 | 4} | {Convert.ToString((6 | 4), 2)}");
+
+            Console.WriteLine($"6 ^ 4 = {6 ^ 4} | {Convert.ToString((6 ^ 4), 2)}");
+
+            Console.WriteLine($"6 << 4 = {6 << 4} | {Convert.ToString((6 << 1), 2)}");
+
+            Console.WriteLine($"6 >> 4 = {6 >> 4} | {Convert.ToString((6 >> 1), 2)}");
+
+            Console.WriteLine($"~6 = {~6} | {Convert.ToString(~((short)6), 2)}");
+
+            Console.WriteLine($"Int.MaxValue {Convert.ToString((int.MaxValue), 2)}");
+
+            Console.WriteLine(Environment.NewLine);
+
+            ContactPreferenceEnum emailAndPhone = ContactPreferenceEnum.Email | ContactPreferenceEnum.Phone;
+
+            Console.WriteLine($"None? {(emailAndPhone | ContactPreferenceEnum.None) == emailAndPhone}");
+
+            Console.WriteLine($"Email? {(emailAndPhone | ContactPreferenceEnum.Email) == emailAndPhone}");
+
+            Console.WriteLine($"Phone? {(emailAndPhone | ContactPreferenceEnum.Phone) == emailAndPhone}");
+
+            Console.WriteLine($"Ponyexpress? {(emailAndPhone | ContactPreferenceEnum.Ponyexpress) == emailAndPhone}");
+        }
+    }
+}
